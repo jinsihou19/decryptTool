8e343d0da19265e7c38d174e3cc8b97550262148
vito <vito@finereport.com>
2016-02-02 17:52:24 +0800

    修复支持拖动文件显示之后，出现无法使用复制粘贴的bug

--- src/tool.java
+++ src/tool.java
@@ -1,11 +1,17 @@
+import com.fr.general.ComparatorUtils;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
+import javax.swing.plaf.UIResource;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.im.InputContext;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -29,45 +35,18 @@ public class tool {
     private JCheckBox tab2CheckBox;
     private File tempFile = null;
     private File configFile = null;
+    private static String TAB1 = "tab1";
+    private static String TAB2 = "tab2";
 
     public tool() {
         tab1SaveBtn.setEnabled(false);
         tab2SaveBtn.setEnabled(false);
-        tab1TextArea.setDragEnabled(true);
-        tab1TextArea.setTransferHandler(new TransferHandler() {
-            private static final long serialVersionUID = 1L;
-
-            public boolean importData(JComponent c, Transferable t) {
-                try {
-                    List<File> fileList = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
-                    File file = fileList.get(0);
-                    if (file.isFile()) {
-                        if (!file.getName().endsWith(".xml")) {
-                            tempFile = file;
-                            tab1url.setText(file.getAbsolutePath());
-                            String text = DecryptTool.decryptToString(file.getAbsoluteFile());
-                            tab1TextArea.setText(text);
-                            tab1SaveBtn.setEnabled(true);
-                        } else {
-                            tab1TextArea.setText("配置文件请使用插件配置文件查看。");
-                        }
-                    }
-                    return true;
-                } catch (UnsupportedFlavorException ufe) {
-                    ufe.printStackTrace();
-                    return true;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-
-            public boolean canImport(JComponent c, DataFlavor[] flavors) {
-                return true;
-            }
-        });
 
         //Tab1内容开始
+        tab1TextArea.setDragEnabled(true);
+        tab1TextArea.setFont(new Font("微软雅黑", Font.PLAIN,14));
+        tab1TextArea.setTransferHandler(new DefaultTransferHandler(TAB1));
+
         openFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -78,17 +57,7 @@ public class tool {
                 jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
                 jfc.showDialog(new JLabel(), "打开");
                 File file = jfc.getSelectedFile();
-                if (file.isFile()) {
-                    if (!file.getName().endsWith(".xml")) {
-                        tempFile = file;
-                        tab1url.setText(file.getAbsolutePath());
-                        String text = DecryptTool.decryptToString(file.getAbsoluteFile());
-                        tab1TextArea.setText(text);
-                        tab1SaveBtn.setEnabled(true);
-                    } else {
-                        tab1TextArea.setText("配置文件请使用插件配置文件查看。");
-                    }
-                }
+                doInTabOne(file);
             }
         });
         tab1SaveBtn.addActionListener(new ActionListener() {
@@ -118,38 +87,8 @@ public class tool {
 
         //以下是Tab2的内容
         tab2TextArea.setDragEnabled(true);
-        tab2TextArea.setTransferHandler(new TransferHandler() {
-            private static final long serialVersionUID = 1L;
-            public boolean importData(JComponent c, Transferable t) {
-                try {
-                    List<File> fileList = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
-                    File file = fileList.get(0);
-                    if (file.isFile()) {
-                        configFile = file;
-                        tab2Url.setText(file.getAbsolutePath());
-                        String text = DecryptTool.readPluginConfig(file);
-                        if (DecryptTool.configIsLock(text)) {
-                            text = DecryptTool.decryptConfig(text);
-
-                            tab2CheckBox.setSelected(true);
-                        }
-                        tab2TextArea.setText(text);
-                        tab2SaveBtn.setEnabled(true);
-                    }
-                    return true;
-                } catch (UnsupportedFlavorException ufe) {
-                    ufe.printStackTrace();
-                    return true;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-
-            public boolean canImport(JComponent c, DataFlavor[] flavors) {
-                return true;
-            }
-        });
+        tab2TextArea.setFont(new Font("微软雅黑", Font.PLAIN,14));
+        tab2TextArea.setTransferHandler(new DefaultTransferHandler(TAB2));
 
         tab2OpenFile.addActionListener(new ActionListener() {
             @Override
@@ -161,18 +100,7 @@ public class tool {
                 jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
                 jfc.showDialog(new JLabel(), "打开插件配置文件");
                 File file = jfc.getSelectedFile();
-                if (file.isFile()) {
-                    configFile = file;
-                    tab2Url.setText(file.getAbsolutePath());
-                    String text = DecryptTool.readPluginConfig(file);
-                    if (DecryptTool.configIsLock(text)) {
-                        text = DecryptTool.decryptConfig(text);
-
-                        tab2CheckBox.setSelected(true);
-                    }
-                    tab2TextArea.setText(text);
-                    tab2SaveBtn.setEnabled(true);
-                }
+                doInTabTwo(file);
             }
         });
 
@@ -199,6 +127,132 @@ public class tool {
         //Tab2内容结束
     }
 
+    public void doInTabTwo(File file) {
+        if (file != null && file.isFile()) {
+            configFile = file;
+            tab2Url.setText(file.getAbsolutePath());
+            String text = DecryptTool.readPluginConfig(file);
+            if (DecryptTool.configIsLock(text)) {
+                text = DecryptTool.decryptConfig(text);
+                tab2CheckBox.setSelected(true);
+            }
+            tab2TextArea.setText(text);
+            tab2SaveBtn.setEnabled(true);
+        }
+    }
+
+    public void doInTabOne(File file) {
+        if (file != null && file.isFile()) {
+            if (!file.getName().endsWith(".xml")) {
+                tempFile = file;
+                tab1url.setText(file.getAbsolutePath());
+                String text = DecryptTool.decryptToString(file.getAbsoluteFile());
+                tab1TextArea.setText(text);
+                tab1SaveBtn.setEnabled(true);
+            } else {
+                tab1TextArea.setText("配置文件请使用插件配置文件查看。");
+            }
+        }
+    }
+
+    class DefaultTransferHandler extends TransferHandler implements
+            UIResource {
+        private String tabName;
+
+        public DefaultTransferHandler(String tab) {
+            tabName = tab;
+        }
+
+        public void exportToClipboard(JComponent comp, Clipboard clipboard,
+                                      int action) throws IllegalStateException {
+            if (comp instanceof JTextComponent) {
+                JTextComponent text = (JTextComponent) comp;
+                int p0 = text.getSelectionStart();
+                int p1 = text.getSelectionEnd();
+                if (p0 != p1) {
+                    try {
+                        Document doc = text.getDocument();
+                        String srcData = doc.getText(p0, p1 - p0);
+                        StringSelection contents = new StringSelection(srcData);
+
+                        // this may throw an IllegalStateException,
+                        // but it will be caught and handled in the
+                        // action that invoked this method
+                        clipboard.setContents(contents, null);
+
+                        if (action == TransferHandler.MOVE) {
+                            doc.remove(p0, p1 - p0);
+                        }
+                    } catch (BadLocationException ble) {
+                    }
+                }
+            }
+        }
+
+        public boolean importData(JComponent comp, Transferable t) {
+            try {
+                if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    List<File> fileList = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
+                    File file = fileList.get(0);
+                    if (ComparatorUtils.equals(tabName, TAB1)) {
+                        doInTabOne(file);
+                    } else {
+                        doInTabTwo(file);
+                    }
+                    return true;
+                }
+            } catch (UnsupportedFlavorException ufe) {
+                ufe.printStackTrace();
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            DataFlavor flavor = getFlavor(t.getTransferDataFlavors());
+
+            if (flavor != null) {
+                InputContext ic = comp.getInputContext();
+                if (ic != null) {
+                    ic.endComposition();
+                }
+                try {
+                    String data = (String) t.getTransferData(flavor);
+
+                    ((JTextComponent) comp).replaceSelection(data);
+                    return true;
+                } catch (UnsupportedFlavorException ufe) {
+                } catch (IOException ioe) {
+                }
+            }
+            return false;
+        }
+
+        public boolean canImport(JComponent comp,
+                                 DataFlavor[] transferFlavors) {
+//            JTextComponent c = (JTextComponent) comp;
+//            if (!(c.isEditable() && c.isEnabled())) {
+//                return false;
+//            }
+//            return (getFlavor(transferFlavors) != null);
+            return true;
+        }
+
+        public int getSourceActions(JComponent c) {
+            return NONE;
+        }
+
+        private DataFlavor getFlavor(DataFlavor[] flavors) {
+            if (flavors != null) {
+                for (DataFlavor flavor : flavors) {
+                    if (flavor.equals(DataFlavor.stringFlavor)) {
+                        return flavor;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
         new tool().init();
     }
@@ -207,7 +261,7 @@ public class tool {
         JFrame frame = new JFrame("工具");
         frame.setContentPane(new tool().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        frame.setSize(400, 500);
         frame.setVisible(true);
     }
 }
